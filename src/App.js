08062a602efb40b5053b0c4d1a5df184089bc104
Shividecode <Shividecode1999.github.com>
2020-03-31 12:42:38 +0530

    Added files

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import { CardList }  from './components/cards/card-list.components';
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <CardList monsters={this.state.monsters} />
+       
+      </div>
+    );
+  }
+}
+export default App;
